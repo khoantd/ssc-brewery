486a9510099460f977859ee51cd20d4b55ee2aa8
Khoa Nguyen <khoa0702@gmail.com>
2024-01-01 20:40:11 +0700

    modify test files

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -14,7 +14,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest
 public class BeerControllerIT extends BaseIT{
 
-    @WithMockUser("spring")
+    @Test
+    void initCreationForm() throws Exception{
+        mockMvc.perform(get("/beers/new").with(httpBasic("user","password")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/createBeer"))
+                .andExpect(model().attributeExists("beer"));
+    }
     @Test
     void findBeers() throws Exception{
         mockMvc.perform(get("/beers/find"))
